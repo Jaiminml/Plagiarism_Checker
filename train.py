ad9cc8a0e8a156721e99f2dcdd2e62c62f9ebb25
Jaiminml <61429107+Jaiminml@users.noreply.github.com>
2021-08-20 12:40:07 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,60 @@
+from transformers import LongformerTokenizerFast, LongformerForSequenceClassification, Trainer, TrainingArguments, LongformerConfig
+import torch
+import numpy as np
+
+from sentence_splitter import SentenceSplitter, split_text_into_sentences
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+
+config = LongformerConfig()
+tokenizer = LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096', max_length = 1024)
+model = LongformerForSequenceClassification.from_pretrained('jaimin/plagiarism_checker')
+
+splitter = SentenceSplitter(language='en')
+#Create a list of sentences from a text file
+def create_list(text):
+    #with open(text, 'r',encoding='utf-8') as file:
+        #data = file.read().replace('\n', '')
+        #print(data)
+    data = text
+    sentence_list = splitter.split(data)
+    return sentence_list
+
+def create_prediction(text):
+    sentence_list = create_list(text)
+    final_output = []
+    for i in range(len(sentence_list)):
+        tokenized_sentence = tokenizer.encode(sentence_list[i])
+        input_ids = torch.tensor([tokenized_sentence])
+        with torch.no_grad():
+            output = model(input_ids)
+        label_indices = np.argmax(output[0].to('cpu').numpy(), axis=1)
+        #print(int(label_indices))
+        final_output.append(int(label_indices))
+    count_1 = final_output.count(1)
+    count_0 = final_output.count(0)
+    pct_plagiarism = (count_1 / len(final_output)) * 100
+
+    return str(pct_plagiarism) + '%' + ' ' + 'Plagiarised'
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    text = request.json["text"]
+    try:
+        out = create_prediction(text)
+        return jsonify({"result":out})
+    except Exception as e:
+        print(e)
+        return jsonify({"result":"Model Failed"})
+
+if __name__ == "__main__":
+    app.run('0.0.0.0',port=5000)
